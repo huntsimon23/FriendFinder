494b193be944c1a0d92e3c64f86a089e27698e8b
huntsimon23 <hunt.simon23@gmail.com>
2019-05-24 20:01:48 -0400

    friend.js populated

---
+++ app/data/friend.js
@@ -0,0 +1,62 @@
+var friend = [
+    {
+    name: "Star Lord",
+    photo: "",
+    scores: [1,3,1,1,1,2,1,3,5,5]
+}, {
+    name: "Nebula",
+    photo: "",
+    scores: [3,4,5,1,1,2,1,1,5,1]
+}, {
+    name: "Dr. Strange",
+    photo: "",
+        scores: [1,5,5,1,2,3,1,1,1,2]
+}, {
+    name: "Black Panther",
+    photo: "",
+    scores: [4,4,3,3,5,2,5,3,2,5]
+}, {
+    name: "Captain America",
+    photo: "",
+    scores: [4,3,3,3,5,5,4,4,2,5]
+}, {
+    name: "Thor",
+    photo: "",
+    scores: [5,3,5,1,2,5,1,5,2,3]
+}, {
+    name: "Hawkeye/Ronin",
+    photo: "",
+    scores: [2,4,2,5,5,3,1,2,1,3]
+}, {
+    name: "Black Widow",
+    photo: "",
+    scores: [2,5,2,5,5,3,1,1,5,2]
+}, {
+    name: "Spider Man",
+    photo: "",
+    scores: [3,1,4,5,5,1,1,4,4,5]
+}, {
+    name: "Iron Man",
+    photo: "",
+    scores: [5,5,2,5,5,3,4,1,5,5]
+}, {
+    name: "Hulk",
+    photo: "",
+    scores: [5,1,5,3,5,4,1,5,2,1]
+}, {
+    name: "Roadie",
+    photo: "",
+    scores: [4,3,1,5,5,3,5,1,2,4]
+}, {
+    name: "Captain Marvel",
+    photo: "",
+    scores: [5,3,5,3,2,3,2,2,2,5]
+}, {
+    name: "Rocket",
+    photo: "",
+    scores: [3,3,4,1,1,2,1,3,5,1]
+}, {
+    name: "Scarlet Witch",
+    photo: "",
+    scores: [3,5,5,5,5,2,1,5,5,2]
+}]
