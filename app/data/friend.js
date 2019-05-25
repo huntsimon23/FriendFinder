c02d66077b32eaa2b50ae838b356dc68e6d9dd73
huntsimon23 <hunt.simon23@gmail.com>
2019-05-24 22:47:19 -0400

    html layouts rough

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,62 +1,62 @@
 var friend = [
     {
     name: "Star Lord",
-    photo: "",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/starlordinfinitywar.png",
     scores: [1,3,1,1,1,2,1,3,5,5]
 }, {
     name: "Nebula",
-    photo: "",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/nebulaendgame.png",
     scores: [3,4,5,1,1,2,1,1,5,1]
 }, {
     name: "Dr. Strange",
-    photo: "",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/doctorstrangeinfinitywar.png",
         scores: [1,5,5,1,2,3,1,1,1,2]
 }, {
     name: "Black Panther",
-    photo: "",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/panther.jpg",
     scores: [4,4,3,3,5,2,5,3,2,5]
 }, {
     name: "Captain America",
-    photo: "",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/captainamerica_endgame.png",
     scores: [4,3,3,3,5,5,4,4,2,5]
 }, {
     name: "Thor",
-    photo: "",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/thorendgame.png",
     scores: [5,3,5,1,2,5,1,5,2,3]
 }, {
     name: "Hawkeye/Ronin",
-    photo: "",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/roninendgame.png",
     scores: [2,4,2,5,5,3,1,2,1,3]
 }, {
     name: "Black Widow",
-    photo: "",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/blackwidowendgame.png",
     scores: [2,5,2,5,5,3,1,1,5,2]
 }, {
     name: "Spider Man",
-    photo: "",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/mcu_spider_man_ffh.png",
     scores: [3,1,4,5,5,1,1,4,4,5]
 }, {
     name: "Iron Man",
-    photo: "",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/ironman_endgame.png",
     scores: [5,5,2,5,5,3,4,1,5,5]
 }, {
-    name: "Hulk",
-    photo: "",
+    name: "Bruce Banner aka Hulk",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/brucebannerendgame.png",
     scores: [5,1,5,3,5,4,1,5,2,1]
 }, {
     name: "Roadie",
-    photo: "",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/warmachineendgame.png",
     scores: [4,3,1,5,5,3,5,1,2,4]
 }, {
     name: "Captain Marvel",
-    photo: "",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/captainmarvelendgame.png",
     scores: [5,3,5,3,2,3,2,2,2,5]
 }, {
     name: "Rocket",
-    photo: "",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/rocketraccoonendgame.png",
     scores: [3,3,4,1,1,2,1,3,5,1]
 }, {
     name: "Scarlet Witch",
-    photo: "",
+    photo: "https://static.tvtropes.org/pmwiki/pub/images/scarletwitchinfinitywar.png",
     scores: [3,5,5,5,5,2,1,5,5,2]
 }]
